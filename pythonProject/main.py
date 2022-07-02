4103fe1cea71c2037969efc63463b7fed23e21df
mehmetkudsiodaman <104054089+mehmetkudsiodaman@users.noreply.github.com>
2022-07-03 00:21:29 +0300

    Initial commit

---
+++ pythonProject/main.py
@@ -0,0 +1,11 @@
+# Press Ctrl+F8 to toggle the breakpoint.
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    print(f'Hi, {name}!')
+
+
+if __name__ == '__main__':
+    print_hi('AI ERA')
